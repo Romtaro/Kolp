30b8e08b912135f1060aebdbc1665319f59b644c
draden05 <rodolphe.lebrun@ynov.com>
2017-03-08 11:34:56 +0100

    Update 8.03

--- MySideMenuApp/platforms/android/assets/www/cordova_plugins.js
+++ MySideMenuApp/platforms/android/assets/www/cordova_plugins.js
@@ -19,7 +19,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-compat": "1.1.0",
-    "cordova-plugin-geolocation": "2.4.1"
+    "cordova-plugin-geolocation": "2.4.2",
+    "cordova-plugin-whitelist": "1.3.2"
 };
 // BOTTOM OF METADATA
 });
--- MySideMenuApp/www/js/geoapi.js
+++ MySideMenuApp/www/js/geoapi.js
@@ -1,73 +1,120 @@
-/**
- * Created by Max on 09/02/2017.
- */
+import {Page, Platform} from 'ionic/ionic';
 
-if (navigator.geolocation) {
-// l’API est disponible
-} else {
-// Pas de support, proposer une solution alternative ?
-}
-
-function maPosition(position) {
-    var infopos = "Position déterminée :\n";
-    infopos += "Latitude : "+position.coords.latitude +"\n";
-    infopos += "Longitude: "+position.coords.longitude+"\n";
-    infopos += "Altitude : "+position.coords.altitude +"\n";
-    document.getElementById("infoposition").innerHTML = infopos;
-}
-
-if(navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(maPosition);
-
-function surveillePosition(position) {
-    var infopos = "Position déterminée :\n";
-    infopos += "Latitude : "+position.coords.latitude +"\n";
-    infopos += "Longitude: "+position.coords.longitude+"\n";
-    infopos += "Altitude : "+position.coords.altitude +"\n";
-    infopos += "Vitesse  : "+position.coords.speed +"\n";
-    document.getElementById("infoposition").innerHTML = infopos;
-}
-
-var survId = navigator.geolocation.watchPosition(surveillePosition);
-
-function erreurPosition(error) {
-    var info = "Erreur lors de la géolocalisation : ";
-    switch(error.code) {
-        case error.TIMEOUT:
-            info += "Timeout !";
-            break;
-        case error.PERMISSION_DENIED:
-            info += "Vous n’avez pas donné la permission";
-            break;
-        case error.POSITION_UNAVAILABLE:
-            info += "La position n’a pu être déterminée";
-            break;
-        case error.UNKNOWN_ERROR:
-            info += "Erreur inconnue";
-            break;
-    }
-    document.getElementById("infoposition").innerHTML = info;
-}
-
-
-map = new google.maps.Map(document.getElementById('mapp'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.SATELLITE
-});
-
-
+var position;
+var labels = "123456789";
+var labelIndex = 0;
+var nbrmark;
 function initMap() {
-    var myLatLng = {lat: -25.363, lng: 131.044};
+        function maPosition(position) {
+            var infopos = "Position déterminée :\n";
+            infopos += "Latitude : " + position.coords.latitude + "\n";
+            infopos += "Longitude: " + position.coords.longitude + "\n";
+            document.getElementById("infoposition").innerHTML = infopos;
 
-    var map = new google.maps.Map(document.getElementById('mapp'), {
-        zoom: 4,
-        center: myLatLng
-    });
+            if (typeof(Storage) !== "undefined") {
 
+                localStorage.removeItem("loc_maPosition");
+                localStorage.setItem("loc_maPosition", infopos);
+                localStorage.removeItem("lat");
+                localStorage.removeItem("long");
+                var latt = position.coords.latitude;
+                var loon = position.coords.longitude;
+                localStorage.setItem("lat", latt);
+                localStorage.setItem("long", loon);
+            }
+            else
+                {
+                    var infop = "localStorage error - erreurPosition (maPosition)";
+                    document.getElementById("infoss").innerHTML = infop;
+                }
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: position.coords.latitude, lng: position.coords.longitude},
+                zoom: 6,
+                mapTypeId: google.maps.MapTypeId.SATELLITE
+            });
+
+            var lat_json = localStorage.getItem("lat");
+            var long_json = localStorage.getItem("long");
+            var lati = JSON.parse(lat_json);
+            var longi = JSON.parse(long_json);
+
+
+            if ($("#map").length != 0) {
+                var myLatLng = {lat: lati, lng: longi};
+
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 4,
+                    sensor: true,
+                    center: myLatLng
+                });
+                var marker = new google.maps.Marker({
+                    position: myLatLng,
+                    sensor: true,
+                    map: map,
+
+                });
+
+                google.maps.event.addListener(map, 'click', function(event) {
+                    addMarker(event.latLng, map);
+                    var ev_ll = event.latLng;
+                    localStorage.removeItem("loc_eventlatLng");
+                    localStorage.setItem("loc_eventlatLng", ev_ll);
+                });
+
+
+
+            }
+
+
+        }
+
+        navigator.geolocation.getCurrentPosition(maPosition);
+
+}
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+extend();
+    nbrmark++;
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
     var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Hello World!'
+        position: location,
+        label: labels[labelIndex++ % labels.length],
+        map: map
     });
+
+function reset_tab(){
+
+}
+
+function refresh_map() {
+initMap();
+}
+
+function localStorage_del_loc_elatLng () {
+  localStorage.removeItem("loc_eventlatLng");
+}
+
+function localStorage_del_loc_maPosition () {
+  localStorage.removeItem("loc_eventlatLng");
+}
+
+function save_loc_marker () {
+
+}
+
+function del_loc_marker () {
+  localStorage.removeItem("loc_eventlatLng");
+
+}
+
+}
+
+function extend() {
+
+document.getElementById("map").style.height = "625px";
+initMap();
+
 }
