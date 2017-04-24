36a83e1a0fcd7444c715526001fc14f7a41f0130
draden05 <rodolphe.lebrun@ynov.com>
2017-04-24 21:58:26 +0200

    21:58:45 update

--- MySideMenuApp/www/js/backgroundGeo.js
+++ MySideMenuApp/www/js/backgroundGeo.js
@@ -1,4 +1,75 @@
-var bgeo = require('backGeo');
+var position;
+var labels = "12345678910111213141516171819";
+var labelIndex = 0;
+var nbrmark;
 
-var dd = bgeo.backgroundGeolocation();
-console.log(dd);
+
+function getWindowHeight() {
+    var windowHeight=0;
+    if (typeof(window.innerHeight)=='number') {
+        windowHeight=window.innerHeight;
+    } else {
+        if (document.documentElement&& document.documentElement.clientHeight) {
+            windowHeight = document.documentElement.clientHeight;
+        } else {
+            if (document.body&&document.body.clientHeight) {
+                windowHeight=document.body.clientHeight;
+            }
+        }
+    }
+    return windowHeight;
+}
+
+function chg_height() {
+             var ny = getWindowHeight();
+             console.log("Height : " + ny );
+             ny +="px";
+             console.log(ny);
+             document.getElementById("emp_map").style.height = ny;
+        }
+
+
+function initMap() {
+    if(navigator.geolocation) {
+        function maPosition(position) {
+                     var ny = getWindowHeight();
+                     console.log("Height : " + ny );
+                     ny +="px";
+                     console.log(ny);
+                     document.getElementById("emp_map").style.height = ny;
+                     document.getElementById("loader-wait").style.display = "none";
+            var infopos = "Position déterminée :\n";
+            infopos += "\nLatitude : " + position.coords.latitude + "\n";
+            infopos += "\nLongitude: " + position.coords.longitude + "\n";
+            document.getElementById("info").innerHTML = infopos;
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: position.coords.latitude, lng: position.coords.longitude},
+                zoom: 6,
+                mapTypeId: google.maps.MapTypeId.ROAD
+            });
+        }
+
+        navigator.geolocation.getCurrentPosition(maPosition);
+        console.log("navigator.geolocation.getCurrentPosition(maPosition); -- DONE")
+    }
+    else {
+        var infopos = "Position indéterminée - Error API :\n";
+        document.getElementById("info").innerHTML += infopos;
+    }
+
+
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+    nbrmark++;
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    var marker = new google.maps.Marker({
+        position: location,
+        label: labels[labelIndex++ % labels.length],
+        map: map
+    });
+}
+
+}
