daafc82fab8a440cb8e34d003c039ae27a3fbd5c
Romtaro <montgrandiromain@gmail.com>
2017-04-25 22:06:23 +0200

    quelque coms

--- MySideMenuApp/www/js/loader.js
+++ MySideMenuApp/www/js/loader.js
@@ -1,5 +1,5 @@
 
-function loader() {
+function loader() {//faire un temps de pause pour charger les élément x)
 var loader = '<h1 class="loader">  <span>L</span>  <span>O</span>  <span>A</span>  <span>D</span>  <span>I</span>  <span>N</span>  <span>G</span>  <span>.</span> <span>.</span> <span>.</span></h1>';
 document.getElementById("loader-wait").innerHTML = loader;
 //console.log(loader);
